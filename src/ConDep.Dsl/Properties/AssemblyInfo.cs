81a5cdb4ef316a809c6bfdd68c2e6b11f5430fb8
Jon Arild Torresdal <jon@torresdal.net>
2014-07-30 12:00:24 +0200

    Renamed Operations project. Added build script

--- src/ConDep.Dsl/Properties/AssemblyInfo.cs
+++ src/ConDep.Dsl/Properties/AssemblyInfo.cs
@@ -2,18 +2,33 @@ using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using ConDep.Dsl.Properties;
 
 [assembly: AssemblyTitle("ConDep.Dsl")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Note: This package is for extending the ConDep DSL. If you're looking for ConDep to do deployment or infrastructure as code, please use the ConDep package. ConDep is a highly extendable Domain Specific Language for Continuous Deployment, Continuous Delivery and Infrastructure as Code on Windows.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyCompanyAttribute("ConDep")]
-[assembly: AssemblyProductAttribute("")]
-[assembly: AssemblyCopyrightAttribute("Copyright © ConDep 2014")]
-[assembly: AssemblyVersionAttribute("2.0.0.1")]
-[assembly: AssemblyFileVersionAttribute("2.0.0.1")]
+[assembly: AssemblyCompany("ConDep")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Copyright © ConDep 2014")]
+[assembly: AssemblyVersion("2.0.0.1")]
+[assembly: AssemblyFileVersion("2.0.0.1")]
+[assembly: ConDepNugetVersion("2.1.0-beta")]
 [assembly: CLSCompliant(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("9c96458c-b257-4105-8def-5785c229b4c6")]
 [assembly: InternalsVisibleTo("ConDep.Dsl.Tests")]
+
+namespace ConDep.Dsl.Properties
+{
+    public class ConDepNugetVersionAttribute : Attribute
+    {
+        public string Version { get; set; }
+
+        public ConDepNugetVersionAttribute(string version)
+        {
+            Version = version;
+        }
+    }
+}
