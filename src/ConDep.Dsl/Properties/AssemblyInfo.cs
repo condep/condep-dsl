c09fa0c28fea3ee687f4424a8f2a26d4c3f88cd2
Jon Arild Torresdal <jon@torresdal.net>
2014-08-01 12:10:31 +0200

    Added ref to condep.dsl.remote.resources

--- src/ConDep.Dsl/Properties/AssemblyInfo.cs
+++ src/ConDep.Dsl/Properties/AssemblyInfo.cs
@@ -10,8 +10,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("ConDep")]
 [assembly: AssemblyProduct("")]
 [assembly: AssemblyCopyright("Copyright © ConDep 2014")]
-[assembly: AssemblyVersion("3.0.3.*")]
-[assembly: AssemblyFileVersion("3.0.3.*")]
+[assembly: AssemblyVersion("3.0.4.*")]
+[assembly: AssemblyFileVersion("3.0.4.*")]
 [assembly: CLSCompliant(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
