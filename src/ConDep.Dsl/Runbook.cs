7ad6016c2c2e08068d803b2f48ea1149042c6d9f
Jon Arild Torresdal <jon@torresdal.net>
2016-10-13 13:05:48 +0200

    Added support for executing Runbook from another Runbook

--- src/ConDep.Dsl/Runbook.cs
+++ src/ConDep.Dsl/Runbook.cs
@@ -8,5 +8,11 @@ namespace ConDep.Dsl
     public abstract class Runbook
     {
         public abstract void Execute(IOfferOperations dsl, ConDepSettings settings);
+
+        public static void Execute<t>(IOfferOperations dsl, ConDepSettings settings) where t : Runbook, new()
+        {
+            var runbook = new t();
+            runbook.Execute(dsl, settings);
+        }
     }
 }
