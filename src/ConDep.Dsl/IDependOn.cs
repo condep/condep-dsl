518e5d52d6173ae30a0fbf4b7288f225b41dc2e7
Jon Arild Torresdal <jon@torresdal.net>
2017-01-25 18:45:32 +0100

    Added deprecation message when IDependOn is used

--- src/ConDep.Dsl/IDependOn.cs
+++ src/ConDep.Dsl/IDependOn.cs
@@ -1,5 +1,10 @@
-﻿namespace ConDep.Dsl
+﻿using System;
+
+namespace ConDep.Dsl
 {
+    [Obsolete("This will be removed in future versions. " +
+              "Instead of having a Runbook depend on another Runbook, " +
+              "use the static method Runbook.Execute from within another Runbook instead.")]
     public interface IDependOn<T> where T : Runbook
     {
          
